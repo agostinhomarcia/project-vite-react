d2dc48690b36eafb366bc29c7aeab9436e601483
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-09-06 21:11:19 -0300

    changed color

--- src/styles.js
+++ src/styles.js
@@ -1,61 +1,62 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width:100vw;
-    height:100vh;
-    background-color:	#CD5C5C;
-    display:flex;
-    justify-content:center;
-    align-items:center;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(90deg, #fa8072 0%, #ffa07a 81.25%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const ContainerItens = styled.div`
-    background:#fff;
-    padding:25px;
-    border-radius:5px;
-    
-
+  background: #ffffff;
+  padding: 20px;
+  border-radius: 5px;
 `;
 
 export const List = styled.ul`
-    padding:0;
-    margin-top:30px;
+  padding: 0;
+  margin-top: 30px;
 `;
 
 export const ItemList = styled.li`
-    color:#000;
-    font-size:15px;
-    font-weight:400;
-    background: #FFA07A;
-    box-shadow: 1px 4px 10px rgba(0,0,0,0.2);
-    border-radius:5px;
-    height:60px;
-    list-style-type:none;
-    margin-bottom:20px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-
+  color: #000000;
+  font-size: 15px;
+  font-weight: 400;
+  background: #E9967A;
+  box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  height: 60px;
+  list-style-type: none;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Input = styled.input`
-    border: 2px solid rgba(209, 211, 212, 0.4);
-    border-radius: 5px;
-    height:40px;
-    font-size:15px;
-    font-weight:500;
-    outline:none;
-    padding-left:10px;
+   border: 2px solid rgba(209, 211, 212, 0.4);
+  border-radius: 5px;
+  height: 40px;
+  font-weight: 400;
+  font-size: 15px;
+  outline: none;
+  padding-left: 10px;
 `;
 
 export const Button = styled.button`
-    width:85px;
-    height:40px;
-    background-color:#FA8072;
-    border-radius:5px;
-    color:#fff;
-    border:none;
-    cursor: pointer;
-    margin-left:20px;
+   background: #FA8072;
+  border-radius: 5px;
+  height: 40px;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  margin-left: 20px;
+  
+  &:hover {
+    opacity: 0.8;
+  }  
+  &:active {
+    opacity: 0.6;
+  }
 `;
-
